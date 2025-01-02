b036482be26a3cd5b3d15523a118a7bf7ba85ec8
Samuel Moura <samuelmalves1@gmail.com>
2025-01-02 10:42:48 -0300

    orderstatus for orders

--- src/main/java/com/samuel/services/config/TestConfig.java
+++ src/main/java/com/samuel/services/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.samuel.services.config;
 
 import com.samuel.services.entities.Order;
 import com.samuel.services.entities.User;
+import com.samuel.services.entities.enums.OrderStatus;
 import com.samuel.services.repositories.OrderRepository;
 import com.samuel.services.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,9 +27,9 @@ public class TestConfig implements CommandLineRunner {
         User user1 = new User(null, "Maria Brown", "maria@gmail.com", "999999999", "123456");
         User user2 = new User(null, "Alex Green", "alex@gmail.com", "998895676", "123456");
 
-        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1);
-        Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2);
-        Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user1);
+        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, user1);
+        Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, user2);
+        Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.CANCELED, user1);
 
         userRepository.saveAll(Arrays.asList(user1, user2));
         orderRepository.saveAll(Arrays.asList(order1, order2, order3));
